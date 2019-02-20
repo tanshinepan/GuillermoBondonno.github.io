0d293c44779e5a642f428549e492c0795d507355
Guillermo Bondonno <guillermobondonno@gmail.com>
2019-02-20 17:52:21 -0300

    Initial Commit

---
+++ deep-net.py
@@ -0,0 +1,244 @@
+
+# coding: utf-8
+
+# In[17]:
+
+
+import pandas as pd
+from sklearn import preprocessing
+from collections import deque
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM, BatchNormalization
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+import time
+
+import os
+
+
+
+# In[3]:
+
+
+main_df = pd.DataFrame()
+
+
+# In[16]:
+
+
+SEQ_LEN = 60
+RATIO_TO_PREDICT = "LTCUSDT"
+FUTURE_PERIOD_PREDICT = 3
+EPOCHS = 1
+BATCH_SIZE = 64
+NAME = f"{SEQ_LEN}-SEQ-{FUTURE_PERIOD_PREDICT}-PRED-{int(time.time())}"
+
+# In[ ]:
+
+
+def preprocess_df(df):
+    df = df.drop("future",1)
+    
+    for col in df.columns:
+        if col != "target":
+            df[col].replace(to_replace=0, value=1, inplace=True)
+            df[col] = df[col].pct_change()
+            df.dropna(inplace=True)
+            #el problema es que en ltcusd hay un volumen que es 0, entonces el pct_change da infinito
+            #habria que reemplazar todos los ceros en jupyter notebook
+            #hay que volver a agregar XRP y LTC
+            #print(min(df[col].values), max(df[col].values), ("NaN" in df[col].values), col)
+            df[col] = preprocessing.scale(df[col].values)
+    df.dropna(inplace = True)
+
+    
+    
+    sequential_data = []
+    prev_days = deque(maxlen = SEQ_LEN)
+
+    for i in df.values:
+    	prev_days.append([n for n in i[:-1]])
+    	#con ese -1 no se incluye a target
+    	if len(prev_days) == SEQ_LEN:
+    		sequential_data.append([np.array(prev_days), i[-1]])
+
+    random.shuffle(sequential_data)
+
+    buys = []
+    sells = []
+
+    for seq, target in sequential_data:
+    	if target == 0:
+    		sells.append([seq, target])
+    	elif target == 1:
+    		buys.append([seq, target])
+
+    random.shuffle(buys)
+    random.shuffle(sells)
+
+    lower = min(len(buys), len(sells))
+
+    buys = buys[:lower]
+    sells = sells[:lower]
+
+    sequential_data = buys + sells
+
+    random.shuffle(sequential_data)
+
+    X = []
+    y = []
+
+    for seq, target in sequential_data:
+    	X.append(seq)
+    	y.append(target)
+
+    return np.array(X), y
+
+
+# In[4]:
+
+
+main_df = pd.DataFrame()
+ratios = ["BTCUSDT","LTCUSDT", "XRPUSDT", "ETHUSDT"]
+for ratio in ratios:
+    dataset = r"C:\Users\Familia\Bioquimica Guille\crypto_data\crypto_data\{}.csv".format(ratio)
+    df = pd.read_csv(dataset, names = ["time","close","volume"])
+    df.rename(columns={"close": f"{ratio}_close", "volume": f"{ratio}_volume"}, inplace=True)
+    df.set_index("time", inplace=True)
+    df = df[[f"{ratio}_close", f"{ratio}_volume"]]
+    
+    
+    if len(main_df) == 0:
+        main_df = df
+    else:
+        main_df = main_df.join(df)
+    
+
+
+# In[6]:
+
+
+def classify(current, future):
+    if float(future)/float(current) >1.0015 :
+        return 1
+    else:
+        return 0
+
+
+# In[7]:
+
+
+main_df['future'] = main_df[f'{RATIO_TO_PREDICT}_close'].shift(-FUTURE_PERIOD_PREDICT)
+
+
+# In[8]:
+
+
+main_df['target'] = list(map(classify, main_df[f'{RATIO_TO_PREDICT}_close'], main_df['future']))
+
+
+
+# In[9]:
+
+
+times = sorted(main_df.index.values)
+
+
+# In[10]:
+
+
+last_5pc = times[-int(0.05*len(times))]
+
+
+# In[11]:
+
+
+last_5pc
+
+
+# In[12]:
+
+
+validation_main_df = main_df[(main_df.index >= last_5pc)]
+main_df = main_df[(main_df.index < last_5pc)]
+
+
+
+
+# In[13]:
+
+
+#ESOS ERAN WHERE METHODS
+
+
+# In[15]:
+
+
+train_x, train_y = preprocess_df(main_df)
+
+validation_x, validation_y = preprocess_df(validation_main_df)
+
+
+
+
+print(f"train data: {len(train_x)} validation: {len(validation_x)}")
+print(f"Dont buys: {train_y.count(0)}, buys: {train_y.count(1)}")
+print(f"VALIDATION Dont buys: {validation_y.count(0)}, buys: {validation_y.count(1)}")
+
+
+
+def create_model():
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(train_x.shape[1:]), return_sequences = True))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+
+    model.add(LSTM(128, input_shape=(train_x.shape[1:]), return_sequences = True))
+    model.add(Dropout(0.1))
+    model.add(BatchNormalization())
+
+    model.add(LSTM(128, input_shape=(train_x.shape[1:])))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+
+    model.add(Dense(32, activation = "relu"))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(2, activation="softmax"))
+
+    opt = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
+
+    model.compile(loss="sparse_categorical_crossentropy",
+    	          optimizer = opt,
+    	          metrics = ["accuracy"])
+
+    return model
+
+model = create_model()
+
+tensorboard = TensorBoard(log_dir=f"logs/{NAME}")
+
+filepath = "RNN_Final-{epoch:02d}-{val_acc:.3f}"  # unique file name that will include the epoch and the validation acc for that epoch
+checkpoint = ModelCheckpoint("models/{}.model".format(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')) # saves only the best ones
+
+checkpoint_path = r"C:\Users\Familia\Bioquimica Guille\crypto_data\modelo_guardado.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create checkpoint callback
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, 
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+history = model.fit(train_x,
+	train_y,
+	batch_size = BATCH_SIZE,
+	epochs = EPOCHS,
+	validation_data = (validation_x,validation_y))#,
+	#callbacks=[tensorboard, checkpoint, cp_callback])
+
+
+
+print(model.predict(train_x[3]))
+	
---
+++ trading-bot-loop.py
@@ -0,0 +1,157 @@
+import numpy as np
+
+import pandas as pd
+
+from sklearn import preprocessing
+from collections import deque
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM, BatchNormalization
+
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+from binance.client import Client
+import time
+import datetime
+import pickle
+print("done with the imports")
+
+
+client = Client("gBtfuC9KBwTE2HpRP3d5pMU6hcl6Lji1rRfb3WDkA2R0sdz9lD5t5cPMbL1y1lOp", "dYw9hpg6TkGxW2KTOSM8ZUF5veFnNQiw6Dmw7IlFvnj52JgTibbqXPG2zEI2lZLQ")
+train_x = pickle.load(open("train_x_.pickle", "rb"))
+class Account:
+    def __init__(self, **kwargs):
+        self.balance = {}
+        prices = client.get_all_tickers()
+        for coin in prices:
+            self.balance[coin["symbol"]] = 0
+        self.balance["USD"] = 0
+        for symbol in kwargs:
+            self.balance[symbol] = kwargs.get(symbol,0)
+    
+    @staticmethod
+    def get_price(symbol):
+        prices = client.get_all_tickers()
+        for coin in prices:
+            if coin["symbol"] == symbol:
+                return coin["price"]
+        
+    
+    def execute_buy(self, amount, symbol):
+        price = self.get_price(symbol)
+        self.balance["USD"] -= float(price) * amount
+        self.balance[symbol] += amount
+        
+        
+    def execute_sell(self, amount, symbol):
+        price = self.get_price(symbol)
+        self.balance["USD"] += float(price) * amount
+        self.balance[symbol] -= amount
+
+
+
+guille = Account(USD = 10000)
+
+
+
+
+
+
+
+starter_data = []
+btc_starter_data = []
+ltc_starter_data = []
+xrp_starter_data = []
+eth_starter_data = []
+
+klines = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1MINUTE, "10 Feb, 2019")[-60:]
+for minute in klines:
+    btc_starter_data.append([minute[4], minute[5]])
+
+klines = client.get_historical_klines("LTCUSDT", Client.KLINE_INTERVAL_1MINUTE, "10 Feb, 2019")[-60:]
+for minute in klines:
+    ltc_starter_data.append([minute[4], minute[5]])
+
+klines = client.get_historical_klines("XRPUSDT", Client.KLINE_INTERVAL_1MINUTE, "10 Feb, 2019")[-60:]
+for minute in klines:
+    xrp_starter_data.append([minute[4], minute[5]])
+
+klines = client.get_historical_klines("ETHUSDT", Client.KLINE_INTERVAL_1MINUTE, "10 Feb, 2019")[-60:]
+for minute in klines:
+    eth_starter_data.append([minute[4], minute[5]])
+
+
+for n in range(len(btc_starter_data)):
+    starter_data.append(btc_starter_data[n]+ltc_starter_data[n]+xrp_starter_data[n]+eth_starter_data[n])
+
+starter_data = np.reshape(np.array(starter_data), (1,60,8))
+
+
+def create_model():
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(train_x.shape[1:]), return_sequences = True))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+
+    model.add(LSTM(128, input_shape=(train_x.shape[1:]), return_sequences = True))
+    model.add(Dropout(0.1))
+    model.add(BatchNormalization())
+
+    model.add(LSTM(128, input_shape=(train_x.shape[1:])))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+
+    model.add(Dense(32, activation = "relu"))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(2, activation="softmax"))
+
+    opt = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
+
+    model.compile(loss="sparse_categorical_crossentropy",
+    	          optimizer = opt,
+    	          metrics = ["accuracy"])
+
+    return model
+
+model = create_model()
+model.load_weights(r"C:\Users\Familia\Bioquimica Guille\crypto_data\modelo_guardado.ckpt")
+
+
+starttime = time.time()
+print("starting while loop")
+while True:
+	klines_btc = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1MINUTE, "2 minutes ago UTC")[-1]
+	klines_ltc = client.get_historical_klines("LTCUSDT", Client.KLINE_INTERVAL_1MINUTE, "2 minutes ago UTC")[-1]
+	klines_xrp = client.get_historical_klines("XRPUSDT", Client.KLINE_INTERVAL_1MINUTE, "2 minutes ago UTC")[-1]
+	klines_eth = client.get_historical_klines("ETHUSDT", Client.KLINE_INTERVAL_1MINUTE, "2 minutes ago UTC")[-1]
+	now_data = np.array([[klines_btc[4], klines_btc[5]]+[klines_ltc[4], klines_ltc[5]]+[klines_xrp[4], klines_xrp[5]]+[klines_eth[4], klines_eth[5]]])
+	if model.predict_classes(starter_data)[0] == 1:
+		guille.execute_buy((guille.balance["USD"]/100)/float(guille.get_price("LTCUSDT")), "LTCUSDT")
+		print(f"buy executed, current balance is {guille.balance['LTCUSDT']} ltcs and {guille.balance['USD']} dollars")
+		time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+		guille.execute_sell((guille.balance["USD"]/100)/float(guille.get_price("LTCUSDT")), "LTCUSDT")
+		print(f"sell executed, current balance is {guille.balance['LTCUSDT']} ltcs and {guille.balance['USD']} dollars")
+		now_data = np.reshape(now_data, (1,1,8))
+		try:
+			starter_data = np.append(starter_data, now_data, axis=1)
+		except Exception as e:
+			print(starter_data.shape, now_data.shape)
+			raise(e)
+
+		starter_data = np.delete(starter_data, 0, axis=1)
+		
+	elif model.predict_classes(starter_data)[0] == 0:
+		now_data = np.reshape(now_data, (1,1,8))
+		try:
+			starter_data = np.append(starter_data, now_data, axis=1)
+		except Exception as e:
+			print(starter_data.shape, now_data.shape)
+			raise(e)
+		starter_data = np.delete(starter_data, 0, axis=1)
+		print("No trade executed")
+		
+		time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+
+	print(f"minute passed, balance: {guille.balance['USD']} dollars, ltcs: {guille.balance['LTCUSDT']}")
+
